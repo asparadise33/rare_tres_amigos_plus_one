df236900799cdd684f58747cdeea93f109e6a982
asparadise33 <abby.paradise@gmail.com>
2024-11-09 11:41:46 -0600

    update posts working

--- request_handler.py
+++ request_handler.py
@@ -2,7 +2,7 @@ from urllib.parse import urlparse, parse_qs
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
-from views import create_user, login_user, get_all_users, update_user, get_single_user, delete_user, get_all_comments, get_single_comment, create_comment, get_all_posts, get_single_post, create_post, get_all_subscriptions, get_single_subscription, delete_post, create_subscription,update_post
+from views import create_user, login_user, get_all_users, update_user, get_single_user, delete_user, get_all_comments, get_single_comment, create_comment, get_all_posts, get_single_post, create_post, get_all_subscriptions, get_single_subscription, delete_post, create_subscription, update_post
 
 
 class HandleRequests(BaseHTTPRequestHandler):
